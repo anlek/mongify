b8935d7f5239c8e23e28a95fe845ac170346b02f
Andrew Kalek <andrew.kalek@anlek.com>
2010-03-22 16:42:47 -0400

    Added specs for UI

---
+++ spec/mongify/ui_spec.rb
@@ -0,0 +1,55 @@
+require File.dirname(__FILE__) + '/../spec_helper'
+
+describe Mongify::UI do
+  before(:each) do
+    @ui = Mongify::UI
+    @config = Mongify::Config
+    @out = StringIO.new
+    @in = StringIO.new
+    @config.out_stream = @out
+    @config.in_stream = @in
+  end
+  
+  it "should add puts to out stream" do
+    @ui.puts "hello"
+    @out.string.should == "hello\n"
+  end
+  
+  it "should add print to out stream without newline" do
+    @ui.print "hello"
+    @out.string.should == "hello"
+  end
+  
+  it "should fetch gets from in stream" do
+    @in.puts "bar"
+    @in.rewind
+    @ui.gets.should == "bar\n"
+  end
+  
+  it "should gets should return empty string if no input" do
+    @config.in_stream = nil
+    @ui.gets.should == ""
+  end
+  
+  it "should request text input" do
+    @in.puts "bar"
+    @in.rewind
+    @ui.request("foo").should == "bar"
+    @out.string.should == "foo"
+  end
+  
+  it "should ask for yes/no and return true when yes" do
+    @ui.expects(:request).with('foo? [yn] ').returns('y')
+    @ui.ask("foo?").should be_true
+  end
+  
+  it "should ask for yes/no and return false when no" do
+    @ui.stubs(:request).returns('n')
+    @ui.ask("foo?").should be_false
+  end
+  
+  it "should ask for yes/no and return false for any input" do
+    @ui.stubs(:request).returns('aklhasdf')
+    @ui.ask("foo?").should be_false
+  end
+end
