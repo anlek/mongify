d63a1302ff0993e01ebe209ba9f43148e2c089ad
Andrew Kalek <andrew.kalek@anlek.com>
2010-12-28 17:26:44 -0500

    Added type to columns

--- lib/mongify/database/column.rb
+++ lib/mongify/database/column.rb
@@ -7,13 +7,13 @@ module Mongify
     # A column in the sql table
     #
     class Column
-      
-      attr_accessor :name
-      attr_reader :options
+      attr_reader :name, :type, :options
 
-      def initialize(*args)
+      def initialize(name, type=:string, *args)
+        @name = name
+        @type = type
         @options = args.extract_options!.stringify_keys
-        self.name = args[0] unless args.empty?
+        
         self
       end
       
