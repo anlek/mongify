e1ecdeca44bf3db893cf159f5a47fa4dd94e3761
Andrew Kalek <andrew.kalek@anlek.com>
2011-01-10 11:08:37 -0500

    Moved printing of column data into the column class

--- lib/mongify/database/column.rb
+++ lib/mongify/database/column.rb
@@ -5,6 +5,8 @@ module Mongify
     #
     class Column
       attr_reader :name, :type, :options
+      
+      ::AVAILABLE_OPTIONS = ['references', 'default']
 
       def initialize(name, type=:string, *args)
         @name = name
@@ -12,8 +14,48 @@ module Mongify
         @type = type.is_a?(Symbol) ? type : type.to_sym
         @options = args.extract_options!.stringify_keys
         
+        auto_detect!
+        
         self
       end
+      
+      def to_print
+        "column \"#{name}\", :#{type}".tap do |output|
+          output_options = options.map{|k, v| (v == nil) ? nil : ":#{k} => \"#{v}\""}.compact
+          output << ", #{output_options.join(', ')}" unless output_options.blank?
+        end
+      end
+      
+      def method_missing(meth, *args, &blk)
+        method_name = meth.to_s.gsub("=", '')
+        if AVAILABLE_OPTIONS.include?(method_name)
+          class_eval <<-EOF
+                          def #{method_name}=(value)
+                            options['#{method_name}'] = value
+                          end
+                          def #{method_name}
+                            options['#{method_name}']
+                          end
+                        EOF
+          send(meth, *args, &blk)
+        else
+          super(meth, *args, &blk)
+        end
+      end
+      
+      #######
+      private
+      #######
+
+      def auto_detect!
+        case name.downcase
+        when 'id'
+          @type = :key if self.type == :integer
+        when /(.*)_id/
+          self.references = $1.to_s.pluralize unless self.references
+        end
+      end
+      
       
     end
   end
--- spec/mongify/database/column_spec.rb
+++ spec/mongify/database/column_spec.rb
@@ -18,4 +18,52 @@ describe Mongify::Database::Column do
     @column = Mongify::Database::Column.new('first_name', nil)
     @column.type.should == :string
   end
+  
+  context "options" do
+    it "should allow to be set by name" do
+      @column = Mongify::Database::Column.new('first_name')
+      @column.references = "users"
+      @column.references.should == "users"
+    end
+
+    it "should not allow to be set unless they're in the ::AVAILABLE_OPTIONS" do
+      @column = Mongify::Database::Column.new('first_name')
+      lambda { @column.unknown = "users" }.should raise_error(NoMethodError)
+    end
+  end  
+  
+  context "auto_detect" do
+    context "id" do
+      it "should type to key" do
+        @column = Mongify::Database::Column.new('id', :integer)
+        @column.type.should == :key
+      end
+      it "should not set type to key if original type is not integer" do
+        @column = Mongify::Database::Column.new('id', :string)
+        @column.type.should == :string
+      end
+    end
+    
+    it "should detect references" do
+      @column = Mongify::Database::Column.new('user_id', :integer)
+      @column.references.should == "users"
+    end
+  end
+  
+  context :to_print do
+    before(:each) do
+      @column = Mongify::Database::Column.new('first_name', :string)
+    end
+    it "should output column name and type" do
+      @column.to_print.should == %Q[column "first_name", :string]
+    end
+    it "should detect references" do
+      @column = Mongify::Database::Column.new('user_id', :integer)
+      @column.to_print.should == %Q[column "user_id", :integer, :references => "users"]
+    end
+    it "should output nil options" do
+      @column.default = nil
+      @column.to_print.should == %Q[column "first_name", :string]
+    end
+  end
 end
