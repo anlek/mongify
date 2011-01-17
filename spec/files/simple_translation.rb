5c6f568b1d80f9f09263b07777fd2a69ddf4ce26
Andrew Kalek <andrew.kalek@anlek.com>
2011-01-17 13:18:05 -0500

    converted from tabs to spaces

--- spec/files/simple_translation.rb
+++ spec/files/simple_translation.rb
@@ -1,26 +1,26 @@
 table "users" do
-	column "id", :key
-	column "first_name", :string
-	column "last_name", :string
-	column "created_at", :datetime
-	column "updated_at", :datetime
+  column "id", :key
+  column "first_name", :string
+  column "last_name", :string
+  column "created_at", :datetime
+  column "updated_at", :datetime
 end
 
 table "posts" do
-	column "id", :key
-	column "title", :string
-	column "owner_id", :integer, :references => :users
-	column "body", :text
-	column "published_at", :datetime
-	column "created_at", :datetime
-	column "updated_at", :datetime
+  column "id", :key
+  column "title", :string
+  column "owner_id", :integer, :references => :users
+  column "body", :text
+  column "published_at", :datetime
+  column "created_at", :datetime
+  column "updated_at", :datetime
 end
 
 table "comments", :embed_in => :posts, :on => :post_id do
-	column "id", :key
-	column "body", :text
-	column "post_id", :integer, :referneces => :posts
-	column "user_id", :integer, :references => :users
-	column "created_at", :datetime
-	column "updated_at", :datetime
+  column "id", :key
+  column "body", :text
+  column "post_id", :integer, :referneces => :posts
+  column "user_id", :integer, :references => :users
+  column "created_at", :datetime
+  column "updated_at", :datetime
 end
