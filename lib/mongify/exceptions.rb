4e0378792f00258563d4fcbe6c699c6ea0971985
Andrew Kalek <andrew.kalek@anlek.com>
2011-02-14 17:09:56 -0500

    Made all (expected) exceptions stem from MongifyError

--- lib/mongify/exceptions.rb
+++ lib/mongify/exceptions.rb
@@ -1,13 +1,14 @@
 module Mongify
+  class MongifyError < RuntimeError; end
   # File Not Found Exception
-  class FileNotFound < RuntimeError; end
+  class FileNotFound < MongifyError; end
   # Raise when configuration file is missing
   class ConfigurationFileNotFound < FileNotFound; end
   # Raised when Translation file is missing
   class TranslationFileNotFound < FileNotFound; end
   
   # Basic Configuration Error Exception
-  class ConfigurationError < RuntimeError; end
+  class ConfigurationError < MongifyError; end
   # Raise when a sqlConnection is required but not given
   class SqlConnectionRequired < ConfigurationError; end
   # Raised when a noSqlConnection is required but not given
@@ -16,5 +17,5 @@ module Mongify
   class DatabaseColumnExpected < ConfigurationError; end
   
   # Raised when application has no root folder set
-  class RootMissing < RuntimeError; end
+  class RootMissing < MongifyError; end
 end
